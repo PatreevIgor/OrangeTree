62cbb0d5a4c2c52f85a77bba7b4f9686aada9443
PatreewIgor <patreewigor@yandex.ru>
2015-07-31 12:30:19 +0300

    fix task

--- task_orange_tree.rb
+++ task_orange_tree.rb
@@ -1,96 +1,126 @@
 class OrangeTree
-  attr_reader :height, :year, :orangeCount
+  attr_accessor :height, :year, :orange_count
 
   def initialize
     @height = 0
     @year = 0
-    @orangeCount = 0
+    @orange_count = 0
   end
 
-  def oneYearPasses
-    @orangeCount = 0
-    @height += 1
-    @year += 1
-    counting_quantity_orange
+  def life_orange_tree(age, object_class_OrangeTree)
+    one_year_passes
+    puts "Прошло #{age} лет,
+          дерево повзрослело на #{age} лет,
+          ему сейчас #{(age-1).times {object_class_OrangeTree.increase_age_for_year}
+                       object_class_OrangeTree.year} год(а),
+          на нем #{ object_class_OrangeTree.counting_quantity_orange_with_the_new_year} апельсинов,
+          его высота составляет #{ (age-1).times {object_class_OrangeTree.increase_height_for_year}
+                                   object_class_OrangeTree.height} метр,
+          текущее состояние дерева: #{current_condition}"
+  end
+
+  def one_year_passes
+    oranges_fell
+    increase_height_for_year
+    increase_age_for_year
+    counting_quantity_orange_with_the_new_year
     current_condition
   end
 
-  def pickAnOrange
-    if orangeCount == 0
-     'no more oranges for the collection this year'
+  def pick_an_orange
+    if orange_count == 0
+      puts 'Нету больше апельсинок в этом году'
     else
-      @orangeCount -= 1
-      'the orange was delicious'
+      @orange_count -= 1
+      puts "Апельсинка съедена, на дереве осталось #{orange_count} апельсинок"
+
     end
   end
 
-  private
-
-  def counting_quantity_orange
-    year <= 5 ? @orangeCount = 0 : @orangeCount = @year*2 + 10
+  def counting_quantity_orange_with_the_new_year
+    year <= 5 ? @orange_count = 0 : @orange_count = @year*2 + 10
   end
 
   def current_condition
     if year >= 20
-      @orangeCount = 0
+      @orange_count = 0
       @height = 0
-      'Tree dead'
+      'Дерево умерло'
     else
-      'tree grows'
+      'Дерево растет'
     end
   end
+
+  def oranges_fell
+    @orange_count = 0
+  end
+
+  def increase_age_for_year
+    @year += 1
+  end
+
+  def increase_height_for_year
+    @height += 1
+  end
+
 end
 
+puts 'Дерево родилось'
+orange_tree = OrangeTree.new
 
-# puts 'from birth'
-# OrangeTree1 = OrangeTree.new
-# puts "height = #{OrangeTree1.height}"
-# puts "year = #{OrangeTree1.year}"
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
+orange_tree.life_orange_tree(1, orange_tree)
+orange_tree.life_orange_tree(5, orange_tree)
+orange_tree.pick_an_orange
 
-# puts 'through 1 year'
-# puts OrangeTree1.oneYearPasses
-# puts "height = #{OrangeTree1.height}"
-# puts "year = #{OrangeTree1.year}"
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
 
-# puts 'through 5 year'
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts "height = #{OrangeTree1.height}"
-# puts "year = #{OrangeTree1.year}"
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
-# puts OrangeTree1.pickAnOrange
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
-# puts OrangeTree1.pickAnOrange
-# puts OrangeTree1.pickAnOrange
+# puts "height = #{orange_tree.height}"
+# puts "year = #{orange_tree.year}"
+# puts "orange_count = #{orange_tree.orange_count}"
 
-# puts 'through 17 year'
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts "height = #{OrangeTree1.height}"
-# puts "year = #{OrangeTree1.year}"
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts OrangeTree1.oneYearPasses
-# puts "height = #{OrangeTree1.height}"
-# puts "year = #{OrangeTree1.year}"
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
-# puts OrangeTree1.oneYearPasses
-# puts "height = #{OrangeTree1.height}"
-# puts "year = #{OrangeTree1.year}"
-# puts "orangeCount = #{OrangeTree1.orangeCount}"
+# puts 'through 1 year--------'
+# puts orange_tree.one_year_passes
+# puts "height = #{orange_tree.height}"
+# puts "year = #{orange_tree.year}"
+# puts "orange_count = #{orange_tree.orange_count}"
+
+# puts 'through 5 year---------'
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts "height = #{orange_tree.height}"
+# puts "year = #{orange_tree.year}"
+# puts "orange_count = #{orange_tree.orange_count}"
+# puts orange_tree.pick_an_orange
+# puts "orange_count = #{orange_tree.orange_count}"
+# puts orange_tree.pick_an_orange
+# puts orange_tree.pick_an_orange
+
+# puts 'through 17 year----------'
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts "height = #{orange_tree.height}"
+# puts "year = #{orange_tree.year}"
+# puts "orange_count = #{orange_tree.orange_count}"
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts orange_tree.one_year_passes
+# puts "height = #{orange_tree.height}"
+# puts "year = #{orange_tree.year}"
+# puts "orange_count = #{orange_tree.orange_count}"
+# puts orange_tree.one_year_passes
+# puts "height = #{orange_tree.height}"
+# puts "year = #{orange_tree.year}"
+# puts "orange_count = #{orange_tree.orange_count}"
+
